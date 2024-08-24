bb0230cf4586e9e51523bbd19e8af1892bb27207
svenplb <psven595@gmail.com>
2024-08-25 01:36:01 +0200

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <div className="flex items-center justify-center">
         <div className="svg-container">
-          <h1 className=" text-5xl font-mono">Hacked Ideas</h1>
+          <h1 className="font-mono text-4xl sm:text-5xl">Hacked Ideas</h1>
         </div>
       </div>
-      <div className="links-container flex items-center justify-center mt-4">
+
+      <div className="links-container flex flex-col sm:flex-row items-center justify-center mt-4 space-y-2 sm:space-y-0 sm:space-x-4">
         <a href="" className="px-2">
           email
         </a>
-        <span className="separator px-2">|</span>
+        <span className="separator px-2 hidden sm:inline">|</span>
         <a href="https://github.com/svenplb/hacked-ideas" className="px-2">
           contribute
         </a>
-        <span className="separator px-2">|</span>
+        <span className="separator px-2 hidden sm:inline">|</span>
         <a href="https://discord.gg/AV2K9QtHub" className="px-2">
           discord
         </a>
@@ -23,3 +23,4 @@ export default function Home() {
     </main>
   );
 }
+
